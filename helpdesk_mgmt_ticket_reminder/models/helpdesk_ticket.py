3e7b32ae59cd783f8b200499297c291d051a30f6
David Alonso <david.alonso@solvos.es>
2020-12-13 13:09:16 +0100

    [ADD] helpdesk_mgmt_ticket_reminder

---
+++ helpdesk_mgmt_ticket_reminder/models/helpdesk_ticket.py
@@ -0,0 +1,39 @@
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+from odoo import _, fields, models, api
+
+import datetime
+
+
+class HelpdeskTicket(models.Model):
+    _inherit = "helpdesk.ticket"
+
+    @api.model
+    def _send_activity_reminder_all(self):
+        # This process is skipped on weekend
+        if fields.datetime.now().weekday() in (5, 6):
+            return
+        reminder_date = fields.datetime.now() - datetime.timedelta(days=7)
+        tickets = self.search([
+            ('stage_id.closed', '=', False),
+            ('write_date', '<=', reminder_date)])
+        # TODO test data
+        # tickets = self.browse([1869, 1870])
+        if tickets:
+            tickets._send_activity_reminder()
+            tickets._update_activity_reminder()
+
+    def _send_activity_reminder(self):
+        # Standard email notification
+        # TODO res_config_settings parameter
+        Template = self.env.ref(
+            "helpdesk_mgmt_ticket_reminder.email_template_ticket_reminder")
+        for ticket in self:
+            Template.send_mail(ticket.id, force_send=True)
+
+    def _update_activity_reminder(self):
+        for ticket in self:
+            # TODO dummy write_date update
+            ticket.write({
+                "description": ticket.description + \
+                    _("<p>Reminder sent on %s</p>") % fields.date.today()})
