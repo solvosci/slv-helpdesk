de51efae5e893b87d0628db68a644c178eafab36
David Alonso <david.alonso@solvos.es>
2020-12-22 17:36:02 +0100

    [ADD] helpdesk_mgmt_ticket_required_fields

---
+++ helpdesk_mgmt_ticket_required_fields/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Helpdesk Ticket Required Fields",
+    "summary": """
+        Sets some relevant ticket fields as required.
+    """,
+    "version": "12.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "After-Sales",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-helpdesk",
+    "depends": [
+        "helpdesk_type",
+    ],
+    "data": [
+        "views/helpdesk_ticket_views.xml",
+    ],
+}
