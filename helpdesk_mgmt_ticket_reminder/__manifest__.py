a423341b15b29e21262e045f281c2285f1d85fac
Luisa Miguéns (Solvos) <luisa.miguens@solvos.es>
2021-01-19 14:29:17 +0100

    [IMP] helpdesk_mgmt_ticket_reminder: include ticket flag reminder in search view

--- helpdesk_mgmt_ticket_reminder/__manifest__.py
+++ helpdesk_mgmt_ticket_reminder/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": """
         Adds a custom email reminder of ticket inactivity in Helpdesk.
     """,
-    "version": "12.0.2.0.0",
+    "version": "12.0.2.1.0",
     "license": "LGPL-3",
     "category": "After-Sales",
     "author": "Solvos",
