43c3db02154348d8580c0029caf722e9903f7131
Luisa Miguéns (Solvos) <luisa.miguens@solvos.es>
2021-01-21 08:59:05 +0100

    [ADD] helpdesk_mgmt_ticket_tree_decoration

---
+++ helpdesk_mgmt_ticket_tree_decoration/__manifest__.py
@@ -0,0 +1,21 @@
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Helpdesk Ticket Tree View Decoration",
+    "summary": """
+        Adds decoration on tickets tree view. 
+        Put decoration according to ticket stage: 
+        red for unattended tickets, green for closed tickets.
+    """,
+    "version": "12.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "After-Sales",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-helpdesk",
+    "depends": [
+        "helpdesk_mgmt",
+    ],
+    "data": [
+        "views/helpdesk_ticket_views.xml",
+    ],
+}
