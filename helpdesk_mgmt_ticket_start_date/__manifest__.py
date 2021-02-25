3f5e9252653f460754fdc8af1d8ea536d101c898
ChristianSantamaria <christiancameselle@hotmail.com>
2021-02-25 17:44:14 +0100

    [ADD] helpdesk_mgmt_ticket_start_date

---
+++ helpdesk_mgmt_ticket_start_date/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Helpdesk Ticket Start Date",
+    "summary": """
+        Add an editable Start Date field that initializes with today's date
+    """,
+    "version": "12.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "After-Sales",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-helpdesk",
+    "depends": [
+        "helpdesk_mgmt",
+    ],
+    "data": [
+        "views/helpdesk_ticket_views.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+}
