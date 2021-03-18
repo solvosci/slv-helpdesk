1ce39a4e571d0522a75ca712f70eb09a9ccce8da
ChristianSantamaria <christiancameselle@hotmail.com>
2021-03-18 17:38:08 +0100

    [ADD] helpdesk_mgmt_timesheet_restricted

---
+++ helpdesk_mgmt_timesheet_restricted/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Helpdesk Group Timesheet Restricted",
+    "summary": """
+       Adds group that cannot see time data in the timesheet and tickets.
+    """,
+    "version": "12.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "After-Sales",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-helpdesk",
+    "depends": [
+        "helpdesk_mgmt_timesheet_readonly",
+    ],
+    "data": [
+        "security/helpdesk_mgmt_timesheet_restricted.xml",
+        "views/helpdesk_mgmt_timesheet_restricted.xml",
+    ],
+}
