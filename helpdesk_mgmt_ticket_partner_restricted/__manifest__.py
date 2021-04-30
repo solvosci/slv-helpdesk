f13df07cf2873bbeed51d316592bfea36806bae8
ChristianSantamaria <christiancameselle@hotmail.com>
2021-04-30 10:14:43 +0200

    [ADD] helpdesk_mgmt_ticket_partner_restricted

---
+++ helpdesk_mgmt_ticket_partner_restricted/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Helpdesk Ticket Partner Restricted",
+    "summary": """
+        Adds partner restriction based on your company
+    """,
+    "version": "12.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "After-Sales",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-helpdesk",
+    "depends": [
+        "helpdesk_mgmt",
+    ],
+    "data": [
+        "views/helpdesk_ticket.xml",
+    ],
+}
