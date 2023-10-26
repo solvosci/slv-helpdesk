ff7ce8115d7d15a78a8a6bfd03bdbebc7a78b320
Adrian Resua Vidal <resuavidal@gmail.com>
2023-10-26 17:58:28 +0200

    [MIG] helpdesk_mgmt_ticket_tree_recent: Migration to 15.0

--- helpdesk_mgmt_ticket_tree_recent/__manifest__.py
+++ helpdesk_mgmt_ticket_tree_recent/__manifest__.py
@@ -5,13 +5,13 @@
     "summary": """
         Adds group that sorts tickets on tree view by last update date.
     """,
-    "version": "12.0.2.0.0",
+    "version": "15.0.1.0.0",
     "license": "LGPL-3",
     "category": "After-Sales",
     "author": "Solvos",
     "website": "https://github.com/solvosci/slv-helpdesk",
     "depends": [
-        "helpdesk_mgmt_timesheet_readonly",
+        "helpdesk_mgmt",
     ],
     "data": [
         "security/helpdesk_security.xml",
