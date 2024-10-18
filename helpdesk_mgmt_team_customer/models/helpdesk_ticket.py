d14fe104c58bca2cc0113e7b548cb5982fc1f2bf
Dante Pereyra <dante.pereyra@solvos.es>
2024-10-18 10:44:11 +0200

    [IMP] helpdesk_mgmt_team_customer: allowed partners for tickets
    Deleted unnecesary imports

--- helpdesk_mgmt_team_customer/models/helpdesk_ticket.py
+++ helpdesk_mgmt_team_customer/models/helpdesk_ticket.py
@@ -1,6 +1,6 @@
 #    Copyright (C) 2024 Solvos Consultoría Informática <www.solvos.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import models,fields, api
+from odoo import models, api
 
 class HelpdeskTicket(models.Model):
     _inherit="helpdesk.ticket"
--- helpdesk_mgmt_team_customer/models/helpdesk_ticket_team.py
+++ helpdesk_mgmt_team_customer/models/helpdesk_ticket_team.py
@@ -1,6 +1,6 @@
 #    Copyright (C) 2024 Solvos Consultoría Informática <www.solvos.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import models, fields, api
+from odoo import models, fields
 
 class HelpdeskTicketTeam(models.Model):
     _inherit="helpdesk.ticket.team"
