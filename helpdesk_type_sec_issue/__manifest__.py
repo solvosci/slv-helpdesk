2bc18ab35760a2547097cdeb85b3df539919661e
David Alonso // Solvos <david.alonso@solvos.es>
2026-04-30 18:33:10 +0200

    [ADD] helpdesk_type_sec_issue
    Adds an extra option for types that enables adding danger level and
    impact new mandatory fields for tickets.

---
+++ helpdesk_type_sec_issue/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2026 Solvos Consultoría Informática (<https://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Helpdesk Type - special types for security issues",
+    "summary": """
+        Adds an extra option for types that enables adding danger level and impact new mandatory fields for tickets
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "After-Sales",
+    "website": "https://github.com/solvosci/slv-helpdesk",
+    "depends": ["helpdesk_type"],
+    "data": [
+        "views/helpdesk_ticket_type_views.xml",
+        "views/helpdesk_ticket_views.xml",
+    ],
+    "installable": True,
+}
---
+++ helpdesk_type_sec_issue/models/__init__.py
@@ -0,0 +1,2 @@
+from . import helpdesk_ticket_type
+from . import helpdesk_ticket
